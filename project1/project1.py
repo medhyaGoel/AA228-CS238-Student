bebc8e7f7e771ca7fea14b86b7af8150016bafcd
Medhya Goel <71662879+medhyaGoel@users.noreply.github.com>
2025-10-17 00:27:40 -0700

    using write_gph + initial score scaffolding

--- project1/project1.py
+++ project1/project1.py
@@ -1,7 +1,8 @@
 import sys
 
-import networkx
-
+import networkx as nx
+import math
+import pandas as pd
 
 def write_gph(dag, idx2names, filename):
     with open(filename, 'w') as f:
@@ -9,11 +10,30 @@ def write_gph(dag, idx2names, filename):
             f.write("{}, {}\n".format(idx2names[edge[0]], idx2names[edge[1]]))
 
 
+def bayesian_score(graph, data):
+    score = 0
+    for node_ind in range(len(graph.nodes())):
+        for parent_config in graph[node_ind].parents():
+            aijo = 0
+            mijo = 0
+            for value in graph[node_ind].values():
+                aijo += value
+                mijo += value
+            score += math.log(math.gamma(aijo)/math.gamma(aijo + mijo))
+            for value in graph[node_ind].values():
+                aijk = 0
+                mijk = 0
+                score += math.log(math.gamma(aijk + mijk)/math.gamma(aijk))
+    return score
+
 def compute(infile, outfile):
-    # WRITE YOUR CODE HERE
-    # FEEL FREE TO CHANGE ANYTHING ANYWHERE IN THE CODE
-    # THIS INCLUDES CHANGING THE FUNCTION NAMES, MAKING THE CODE MODULAR, BASICALLY ANYTHING
-    pass
+    df = pd.read_csv(infile)
+    D = nx.DiGraph()
+    for column_name in df.columns:
+        D.add_node(column_name)
+    # k2_algo() || local_search_restarts || local_search_simulated_annealing
+    idx2names = {i: name for i, name in enumerate(df.columns)}
+    write_gph(D, idx2names, outfile)
 
 
 def main():
